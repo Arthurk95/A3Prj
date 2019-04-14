186e68cdde66deb209e768b897259e4f17c09562
Arthur Kharit <arthurkharit@csus.edu>
2019-04-14 15:52:54 -0700

    Sets a fixed objects toBeMoved value to true if it's selected when Position button is pressed

---
+++ src/com/mycompany/a3/commands/CMDPosition.java
@@ -0,0 +1,34 @@
+package com.mycompany.a3.commands;
+
+import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import com.mycompany.a3.gameobject.Fixed;
+import com.mycompany.a3.gameobject.GameObject;
+import com.mycompany.a3.gameobject.objectcollection.IIterator;
+import com.mycompany.a3.gameworld.GameWorld;
+
+public class CMDPosition extends Command{
+	private GameWorld gameWorld;
+	
+	public CMDPosition(GameWorld gw) {
+		super("Position");
+		gameWorld = gw;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ev) {
+		if(gameWorld.isPaused()) {
+			IIterator allObjects = gameWorld.getObjectCollection().getIterator();
+			while(allObjects.hasNext()) {
+				GameObject object = (GameObject)allObjects.getNext();
+				if(object instanceof Fixed) {
+					Fixed fixedObj = (Fixed)object;
+					if(fixedObj.isSelected()){
+						fixedObj.willBeMoved();
+						break;
+					}
+				}
+			}
+		}
+	}
+}
